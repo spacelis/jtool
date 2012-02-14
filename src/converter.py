ff753e9f565ecd3d8f9e1941b5b8d65800e2314f
wenli <spacelis@gmail.com>
2012-02-14 19:05:00 +0100

    transfer parameters in commandline into inner classes of converters

--- src/converter.py
+++ src/converter.py
@@ -4,48 +4,55 @@
 Description:
     Manipulate field data.
 History:
+    0.2.0 + Introducing parametered converter with parameters from console
     0.1.0 The first version.
 """
 __version__ = '0.1.0'
 __author__ = 'SpaceLis'
 
 from datetime import datetime
+import re
 import logging
 import argparse
-import json
 from fileset import FileInputSet
 
 import sys
 __M__ = sys.modules[__name__]
 
-class DiscreteLabelCoverter(object):
+CONVERTERNAME = re.compile(r'(?P<name>.*Converter)(\[(?P<para>.*)\])?$')
+
+class DiscreteLabelConverter(object):
     """ Use a set of numbers to form a serious bin bounded by numbers.
         E.g. bins = [1, 2, 3, 4], label= = ['<1', '1..2', '2..3', '3..4', '>4']
         The len(labels) should be one more then len(bins). The order is attained
         for dispatching, be sure they are in chronical order.
     """
-    def __init__(self, bins, labels):
-        super(DiscreteLabelCoverter, self).__init__()
-        if not len(labels) == len(bins) + 1:
+    def __init__(self, paraline):
+        super(DiscreteLabelConverter, self).__init__()
+        self._bins, self._labels = [v.split(',') for v in paraline.split('|')]
+        self._bins = [float(v) for v in self._bins]
+        if not len(self._labels) == len(self._bins) + 1:
             raise ValueError('The len(labels) doesn\'t equals to len(bins)+1')
-        self._bins = bins
-        self._labels = labels
 
-    def __call__(self, item):
+    def __call__(self, val):
         """ return the label for the token
         """
-        # convert the item into the same type
-        item = type(self.bins[0])(item)
-        if item <= self._bins[0]:
-            return self._labels[0]
-        for label, floor in zip(self._labels[1:], self._bins):
-            if item <= floor:
+        # convert the val into the same type
+        val = float(val)
+        for label, floor in zip(self._labels[:-1], self._bins):
+            if val <= floor:
                 return label
+        return self._labels[-1]
 
 def twittertime(timestr):
     """ Convert string format of timestamp in tweets to datetime objects
     """
-    return datetime.strptime(timestr[4:], '%b %d %H:%M:%S +0000 %Y')
+    try:
+        return datetime.strptime(timestr[4:], '%b %d %H:%M:%S +0000 %Y')
+    except ValueError as e:
+        logging.error(e)
+        exit(1)
+
 
 def TwitterTimeConverterFactory(fmt, converter=lambda x: x):
     """ Return a labeler according to time format
@@ -68,8 +75,14 @@ class Pipeline(object):
         self._cv_list = cv_list
         self._cv_pipeline = list()
         for cvname in cv_list:
-            if cvname.endswith('Converter'):
-                self._cv_pipeline.append(getattr(__M__, cvname))
+            m = CONVERTERNAME.match(cvname)
+            if m.group('name').endswith('Converter'):
+                if m.group('para'):
+                    self._cv_pipeline.append(getattr(__M__, m.group('name'))(m.group('para')))
+                    logging.debug('[Converter] %s %s' % (m.group('name'), m.group('para')))
+                else:
+                    self._cv_pipeline.append(getattr(__M__, m.group('name')))
+                    logging.debug('[Converter] %s' % (m.group('name'),))
             else:
                 raise ValueError('No labeler named %s found' % (cvname,))
 
@@ -108,14 +121,13 @@ def parse_arg():
     parser = argparse.ArgumentParser(description='Simple Text Converter with support to fields.',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             epilog='Available Converter:\n' + cvnames)
-    parser.add_argument('-p', '--pipeline', action='append', dest='pipelines',
-            metavar='labeler',
-            help='Tokenize the input line before doing statistics, useful for '
-            'text.')
+    parser.add_argument('-f', '--fields', action='append', dest='fields',
+            metavar='labeler', default=list(),
+            help='Specifying a pipeline should be used on a field.')
     parser.add_argument('-d', '--delimiter', action='store', dest='delimiter',
-            default='\t', help='The separator of input data')
-    parser.add_argument('-j', '--json', action='store_true', default=False, dest='json',
-            help='Use json format as input')
+            default='\t', help='The delimiter of input and output data format.')
+    parser.add_argument('--debug', action='store_true', dest='debug', default=False,
+            help='Run converter in debug mode.')
     parser.add_argument('sources', metavar='file', nargs='*',
             help='Files as inputs. STDIN will be used, if no input file specified.')
     return parser.parse_args()
@@ -124,7 +136,7 @@ def main():
     """ Console command
     """
     args = parse_arg()
-    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG if args.debug else logging.WARNING)
     logging.debug(args)
 
 	# Determine the input of JSON streams
@@ -134,25 +146,14 @@ def main():
         fin = sys.stdin
 
     fproc = FieldProcesser()
-    if args.json and args.pipelines:
-        for p in args.pipelines:
-            field, plname = p.split(':', 1)
-            fproc.add_field_converter(field, Pipeline(plname.split(':')))
-    elif args.pipelines:
-        for p in args.pipelines:
-            field, plname = p.split(':', 1)
-            fproc.add_field_converter(int(field), Pipeline(plname.split(':')))
+    for p in args.fields:
+        field, plname = p.split(':', 1)
+        fproc.add_field_converter(int(field), Pipeline(plname.split(':')))
 
-    if args.json:
-        for line in fin:
-            jobj = json.loads(line)
-            njobj = fproc.process(jobj)
-            print >> sys.stdout, njobj.dumps()
-    else:
-        for line in fin:
-            data = line.strip().split(args.delimiter)
-            ndata = fproc.process(data)
-            print >> sys.stdout, args.delimiter.join(ndata)
+    for line in fin:
+        data = line.strip().split(args.delimiter)
+        ndata = fproc.process(data)
+        print >> sys.stdout, args.delimiter.join(ndata)
 
 def test():
     """docstring for test
