f7cfd0f29142406f2b1fad6ad3d9f6538db08cd9
wenli <spacelis@gmail.com>
2012-02-14 13:40:20 +0100

    move converters out and introducing fields combination

--- src/stats.py
+++ src/stats.py
@@ -12,119 +12,103 @@ __version__ = '0.1.1'
 __author__ = 'SpaceLis'
 
 import argparse
-import re
 import sys
 import logging
 import json
 from fileset import FileInputSet
-from labeler import Pipeline
-
-_SPACES = re.compile(r'\s+')
-__M__ = sys.modules['labeler']
-
-def SpaceTokenizer():
-    return _SPACES.split
-
-class FieldConverter(object):
-    """ Preprocess the field data before doing statistics
-    """
-    def __init__(self):
-        super(FieldConverter, self).__init__()
-        self.processer = dict()
-
-    def add_field_processer(self, field, pipeline):
-        """ Add a new process to a field
-        """
-        self.processer[field] = pipeline
-
-    def process(self, datum):
-        """ Process a line of data
-        """
-        logging.debug('Processing data')
-        for f in self.processer.iterkeys():
-            logging.debug('Processing [%s]' % (f,))
-            datum[f] = self.processer[f](datum[f])
-        return datum
 
 
-def discrete_statistics(instream, fproc, args):
+def discrete_statistics(instream, args):
     """ Do statistics on a searious dicrete tokens
     """
     stat = dict()
 
     cnt = 0
-    if args.json:
+    def add_token(token):
+        if token in stat:
+            stat[token] += 1
+        else:
+            stat[token] = 1
+
+    if args.intype == 'json':
         for line in instream:
             cnt += 1
             try:
-                if args.pipelines:
-                    bdatum = json.loads(line)
-                    datum = fproc.process(bdatum)
-                    token = json.dumps(datum)
-                    logging.debug('%s => %s' % (datum, token))
-                else:
-                    token = line.strip()
-                if token in stat:
-                    stat[token] += 1
-                else:
-                    stat[token] = 1
+                jobj = json.loads(line)
+                for idx in args.fields:
+                    add_token(jobj[idx])
+            except KeyError as e:
+                logging.error('[%s] Field Index Out of List: %s' % (cnt, str(e)))
+                if not args.ignore_index_error and not args.ignore_error:
+                    exit(1)
+            except ValueError as e:
+                logging.error('Failed at [%s]: %s' % (cnt, str(e)))
+                if not args.ignore_error:
+                    exit(1)
+
+    elif args.intype == 'csv':
+        for line in instream:
+            cnt += 1
+            try:
+                datarow = line.strip().split(args.delimiter)
+                for idx in args.fields:
+                    add_token(datarow[idx])
+            except IndexError as e:
+                logging.error('[%s] Field Index Out of List: %s' % (cnt, str(e)))
+                if not args.ignore_index_error and not args.ignore_error:
+                    exit(1)
             except Exception as e:
-                logging.warn('Failed at [%s]: %s' % (cnt, str(e)))
+                logging.error('Failed at [%s]: %s' % (cnt, str(e)))
+                if not args.ignore_error:
+                    exit(1)
 
     else:
         for line in instream:
             cnt += 1
             try:
-                if args.pipelines:
-                    bdatum = line.strip().split(args.separator)
-                    datum = fproc.process(bdatum)
-                    token = args.separator.join(datum)
-                    logging.debug('%s => %s' % (bdatum, token))
-                else:
-                    token = line.strip()
-                if token in stat:
-                    stat[token] += 1
-                else:
-                    stat[token] = 1
-            except IndexError as e:
-                logging.error('[Pipeline] index out of list')
-                exit(1)
+                datarow = line.strip()
+                add_token(datarow)
             except Exception as e:
-                logging.warn('Failed at [%s]: %s' % (cnt, str(e)))
+                logging.error('Failed at [%s]: %s' % (cnt, str(e)))
+                if not args.ignore_error:
+                    exit(1)
     return stat
 
 def parse_parameter():
     """ Parse parameters from console
     """
-    lbs = ', '.join([lb for lb in __M__.__dict__ if lb.endswith('Labeler')])
 
     parser = argparse.ArgumentParser(description='Simple distribution calculator',
             formatter_class=argparse.RawDescriptionHelpFormatter,
-            epilog='Available Labelers:\n' + lbs)
-    parser.add_argument('-t', '--tokenizer', action='store', dest='tokenizer',
-            help='Tokenize the input line before doing statistics, useful for '
-            'text.')
-    parser.add_argument('-p', '--pipeline', action='append', dest='pipelines',
-            metavar='labeler',
-            help='Tokenize the input line before doing statistics, useful for '
-            'text.')
-    parser.add_argument('-s', '--separator', action='store', dest='separator',
-            default='\t', help='The separator of input data')
-    parser.add_argument('-j', '--json', action='store_true', default=False, dest='json',
-            help='Use json format as input')
-    parser.add_argument('-f', '--sort-freq', action='store_true', default=False, dest='sortf',
+            epilog='''NOTE: If none of input types specified, each entire line
+            will be used as a token''')
+    parser.add_argument('-t', '--input-type', action='store', default=None, dest='intype',
+            help='Choose an alternative input format', choices=['json', 'csv'])
+    parser.add_argument('-f', '--field', action='append', default=None, dest='fields',
+            help='Specifying the fields to be processed.')
+    parser.add_argument('-d', '--delimiter', action='store', dest='delimiter',
+            default='\t', help='The delimiter of input data')
+    parser.add_argument('-F', '--sort-freq', action='store_true', default=False, dest='sortf',
             help='Output the statistics with sorting on frequency.')
-    parser.add_argument('-k', '--sort-key', action='store_true', default=False, dest='sortk',
+    parser.add_argument('-K', '--sort-key', action='store_true', default=False, dest='sortk',
             help='Output the statistics with sorting on key.')
+    parser.add_argument('-X', '--ignore-index-error', action='store_true', default=False,
+            dest='ignore_index_error',
+            help='Ignore the errors when then field index doesn\'t exist')
+    parser.add_argument('-E', '--ignore-error', action='store_true', default=False,
+            dest='ignore_error',
+            help='Ignore all the errors when doing statistics')
     parser.add_argument('sources', metavar='file', nargs='*',
             help='Files as inputs. STDIN will be used, if no input file specified.')
+
     return parser.parse_args()
 
 def main():
     """ main()
     """
     args = parse_parameter()
-    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.basicConfig(format='[%(levelname)s] %(message)s',
+                        level=logging.INFO)
     logging.debug(args)
 
 	# Determine the input of JSON streams
@@ -133,17 +117,14 @@ def main():
     else:
         fin = sys.stdin
 
-    fp = FieldConverter()
-    if args.json and args.pipelines:
-        for p in args.pipelines:
-            field, plname = p.split(':', 1)
-            fp.add_field_processer(field, Pipeline(plname.split(':')))
-    elif args.pipelines:
-        for p in args.pipelines:
-            field, plname = p.split(':', 1)
-            fp.add_field_processer(int(field), Pipeline(plname.split(':')))
+    if args.intype == 'json' and args.fields == None:
+        logging.error('No fields specified for processing for a json input')
+        exit(1)
+    if args.intype == 'csv' and args.fields == None:
+        args.fields = [0,]
+
     # Do statistics
-    stat = discrete_statistics(fin, fp, args)
+    stat = discrete_statistics(fin, args)
 
     # Sort results
     if args.sortf:
